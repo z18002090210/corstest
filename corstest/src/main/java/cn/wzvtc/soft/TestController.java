d870d270206667c40ade39bfc0e625010d5437f4
Your Name <you@example.com>
2019-10-21 09:18:32 +0800

    init commit

--- corstest/src/main/java/cn/wzvtc/soft/TestController.java
+++ corstest/src/main/java/cn/wzvtc/soft/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-//@CrossOrigin
+@CrossOrigin
 @RestController("/" )
 public class TestController {
 
